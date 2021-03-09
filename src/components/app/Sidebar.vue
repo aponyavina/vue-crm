e67511180340326bf4af2558253d399670d99792
aponyavina <ponyavina1997@mail.ru>
2021-03-09 18:14:26 +0300

    фикс бага со скрывающемся сайдбаром

--- src/components/app/Sidebar.vue
+++ src/components/app/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="sidenav app-sidenav" :class="{open: 'value'}">
+    <ul class="sidenav app-sidenav" :class="{open: value}">
 
         <router-link
             v-for='link in links'
